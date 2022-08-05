5dd51992b304e11cee612fafaee996c040328420
Festivemena <88979259+Festivemena@users.noreply.github.com>
2022-08-05 03:18:33 +0100

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,19 @@
-import Sidebar from '../components/Sidebar'
-import Feed from '../components/feed'
-import Widgets from '../components/widgets'
 import Mobile from '../components/Mobile/Home/Mobile'
 
 const style = {
-  wrapper: `flex justify-center  h-full w-full select-none bg-[#222229] text-white`,
+  wrapper: `flex justify-center  h-full w-full select-none bg-black text-white`,
   content: `max-w-[1400px] flex justify-between`, 
-  mobile: `flex justify-center h-full w-full md:hidden select-none bg-[#222229] text-white` 
+  mobile: `flex justify-center h-full w-full md:hidden select-none bg-white text-black` 
 }
 
 export default function Home() {
   return (
- <div className={style.wrapper}>
-   <div className='hidden md:flex'>
-      <div className={style.content}>
-        <Sidebar /> 
-        <Feed /> 
-        <Widgets /> 
-      </div>
-   </div>
   
   <div className={style.mobile}>
 
       <Mobile />
       
     </div>  
- </div>
+
   )
 }
--- pages/profile.js
+++ pages/profile.js
@@ -1,24 +1,22 @@
+import Footer from '../components/Mobile/Home/footer'
 import ProfileHeader from '../components/profile/ProfileHeader'
 import ProfileTweets from '../components/profile/ProfileTweets'
-import Sidebar from '../components/Sidebar'
-import Widgets from '../components/Widgets'
 
 const style = {
-  wrapper: `flex-col justify-center h-full w-full select-none bg-[#222229] text-white`,
+  wrapper: `flex-col justify-center h-full w-full select-none bg-[#FFFFFF] text-white`,
   content: ` flex justify-between`,
-  mainContent: `p-flex-[2] border-r border-l border-[#38444d]`,
+  mainContent: ` border-r border-l border-[#38444d]`,
 }
 
 const profile = () => {
   return (
     <div className={style.wrapper}>
       <div className={style.content}>
-        <div className='hidden md:flex'><Sidebar initialSelectedIcon={'Profile'} /></div>
         <div className={style.mainContent}>
           <ProfileHeader />
           <ProfileTweets />
+         
         </div>
-       <div className='hidden md:flex'> <Widgets /></div> 
       </div>
     </div>
   )
